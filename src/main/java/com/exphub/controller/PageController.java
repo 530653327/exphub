6c4ee0dfacac6d04b377dadfc8bc35c9329cccf0
shiyue <530653327@qq.com>
2026-05-20 12:57:51 +0800

    fix: 添加缺失的 Controller import

--- src/main/java/com/exphub/controller/PageController.java
+++ src/main/java/com/exphub/controller/PageController.java
@@ -8,6 +8,7 @@ import com.exphub.service.AiAssistantService;
 import com.exphub.service.CallLogService;
 import com.exphub.service.DocService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
